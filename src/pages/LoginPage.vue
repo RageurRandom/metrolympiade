d657c086ff6f60ee53f7242f1fe67467a63bbc3f
Leo Berranger <berrang232@unicaen.fr>
2025-04-14 21:21:52 +0200

    begining of login and register pages

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { ref } from "vue";
+
+  const email = ref("");
+  const password = ref("");
+
+  function login() {
+
+  }
+</script>
+
+<template>
+  <main>
+<form @submit.prevent="login">
+  <label for="email">Email :</label>
+  <input type="email" id="email" v-model="email">
+
+  <label for="password">Mot de Passe : </label>
+  <input type="password" id="password" v-model="password">
+  <button>Connexion</button>
+</form>
+  </main>
+</template>
---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,45 @@
+<script setup>
+  import { ref } from "vue";
+
+  const name = ref("");
+  const email = ref("");
+  const password = ref("");
+
+  const teams = [
+    {
+      name: "test 1",
+      id : 1
+    },
+    {
+      name : "test 2",
+      id: 2
+    }
+  ];
+
+  function register() {
+
+  }
+</script>
+
+<template>
+  <main>
+    <form @submit.prevent="register">
+      <label for="name">Nom : </label>
+      <input type="text" id="name" v-model="name">
+
+      <label for="email">Email :</label>
+      <input type="email" id="email" v-model="email">
+
+      <label for="password">Mot de Passe : </label>
+      <input type="password" id="password" v-model="password">
+
+      <label for="team">Equipe : </label>
+      <select id="team" v-for="(teamName, id) in teams"
+      :key="id">
+        <option value="{{id}}">{{ teamName }}</option>
+      </select>
+
+      <button>Connexion</button>
+    </form>
+  </main>
+</template>
