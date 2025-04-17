5ba5fbbbf05067582923446ad57d5d6665dc1ff0
Leo Berranger <berrang232@unicaen.fr>
2025-04-17 19:27:57 +0200

    recuperation des infos

--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -2,18 +2,56 @@
 import { useUserData } from '@/composables/useUserData';
 import { computed, ref } from 'vue';
 import teamMate from '@/components/teamMate.vue';
+import { apiUrl } from '@/main';
 
-  const userInfos = computed(()=>{
-    return useUserData().user;
+  const newTeamName = ref("");
+  const user = useUserData().user; //get the user data from the composable
+
+  const teamMates = new Array();
+  const teamName = ref("");
+  const teamId = ref("");
+
+  const isLoading = ref(true);
+
+  console.log(user.value);
+
+  //Récupération de l'équipe
+  fetch(apiUrl + "/teams/me", {
+    method: "GET",
+    headers: {
+      'Authorization': 'Bearer ' + user.value.token
+    }
+  })
+  .then((res) => {
+    if (!res.ok) throw new Error("Identifiants incorrects");
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    teamMates.length = 0;
+    if(data.members != undefined){
+
+      data.members.forEach(element => {
+        teamMates.push(element);
+      });
+    }
+
+
+    teamName.value = data.name;
+
+    teamId.value = data.id;
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+  .finally(() => {
+    isLoading.value = false;
   });
 
-  const teamMates = ["test1", "test2"];
 
-  console.log(userInfos.value)
-  const newTeamName = ref("")
 
   function removeElt(){
-    console.log("test");
+
   }
 </script>
 
@@ -29,8 +67,8 @@ import teamMate from '@/components/teamMate.vue';
         </div>
 
 
-        <teamMate v-for="teammateName in teamMates" :name="teammateName" @remove="removeElt()"/>
-
+        <teamMate v-if="!isLoading" v-for="teammateName in teamMates" :name="teammateName" @remove="removeElt()"/>
+        <p v-if="isLoading">Chargement . . .</p>
         <div>
           <button class="bg-gray-800 text-white p-2 rounded shadow-lg hover:bg-gray-700 transition-colors duration-300">
             Valider
