24033b2fa0cd20754587b4d7c33235eeda3726f6
tarek <tarekremo64@gmail.com>
2025-04-21 14:31:32 +0200

    fixing userData return value

--- src/composables/useUserData.js
+++ src/composables/useUserData.js
@@ -1,22 +1,22 @@
-import { computed } from 'vue'
+import { ref } from 'vue'
+
+const getLocalStorageData = () => {
+  try {
+    return JSON.parse(localStorage.getItem('user'))
+  } catch (error) {
+    console.error('Error parsing user data:', error)
+    return null
+  }
+}
+
+const user = ref(getLocalStorageData())
+
+const logout = () => {
+  localStorage.removeItem('user')
+  window.location.reload() // To refresh the page and remove user data from the state
+}
 
 export function useUserData() {
-  const getLocalStorageData = () => {
-    try {
-      return JSON.parse(localStorage.getItem('user'))
-    } catch (error) {
-      console.error('Error parsing user data:', error)
-      return null
-    }
-  }
-  
-  const user = computed(() => getLocalStorageData())
-  
-  const logout = () => {
-    localStorage.removeItem('user')
-    window.location.reload() // To refresh the page and remove user data from the state
-  }
-  
   return {
     user,
     logout
